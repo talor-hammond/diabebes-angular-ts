36de606a1df9cdf1e58818a866a0e66599d9a5ad
talor-hammond <talorjhammond@gmail.com>
2018-08-28 22:18:08 +1200

    complete initial Reading model

--- src/app/components/readings/reading.model.ts
+++ src/app/components/readings/reading.model.ts
@@ -1,6 +1,13 @@
 export class Reading {
     public bg: Number
     public time: String
-    public insulin?: Number
     public note: String
+    public insulin?: Number
+
+    constructor(bg: Number, time: String, note: String, insulin?: number) {
+        this.bg = bg
+        this.time = time
+        this.note = note        
+        this.insulin = insulin
+    }
 }
