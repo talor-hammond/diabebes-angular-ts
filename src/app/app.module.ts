88cd9a581a8806082febc0479b5314224be7a963
talor-hammond <talorjhammond@gmail.com>
2018-08-28 20:40:39 +1200

    generated initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ReadingsComponent } from './components/readings/readings.component';
+import { HomeComponent } from './components/home/home.component';
+import { ReadingComponent } from './components/readings/reading/reading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ReadingsComponent,
+    HomeComponent,
+    ReadingComponent
   ],
   imports: [
     BrowserModule
