193bf7a7c1c1675ac2616ffef586a64b2fd1fa4f
talor-hammond <talorjhammond@gmail.com>
2018-08-29 12:11:21 +1200

    new Readings pushed to readings array

--- src/app/components/home/readings/readings.component.ts
+++ src/app/components/home/readings/readings.component.ts
@@ -10,10 +10,7 @@ import { Reading } from './reading.model'
 })
 export class ReadingsComponent implements OnInit {
   name: String
-  // readings: Reading[] = [] // type: array of 'Reading' objects, defaulting to an empty array
-  readings: Reading[] = [
-    new Reading(8, '8:30PM', 0, 'Nothing out of the ordinary; levels steady')
-  ]
+  readings: Reading[] = [] // type: array of 'Reading' objects, defaulting to an empty array
 
   constructor() {
     this.name = 'Talor'
@@ -23,9 +20,7 @@ export class ReadingsComponent implements OnInit {
   }
 
   onReadingAdded(reading: Reading) {
-    console.log('reading')
-
-    console.log(reading)
+    this.readings.push(reading)
   }
 
 }
