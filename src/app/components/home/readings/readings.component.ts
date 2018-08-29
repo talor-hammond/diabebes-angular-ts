4eb304ba1d617bb5819364e3785b33331b55debb
talor-hammond <talorjhammond@gmail.com>
2018-08-29 14:39:16 +1200

    added mock data for readings

--- src/app/components/home/readings/readings.component.ts
+++ src/app/components/home/readings/readings.component.ts
@@ -9,7 +9,7 @@ import { Reading } from './reading.model'
   styleUrls: ['./readings.component.css']
 })
 export class ReadingsComponent implements OnInit {
-  name: String
+  name: string
   // readings: Reading[] = [] // type: array of 'Reading' objects, defaulting to an empty array
   readings: Reading[] = [
     {
@@ -17,6 +17,23 @@ export class ReadingsComponent implements OnInit {
       time: '9:00AM',
       insulin: 4,
       note: '~30grams of carbs for breakfast'
+    },
+    {
+      bg: 12.2,
+      time: '12:30PM',
+      insulin: 2,
+      note: '4 units / 30g could be too low'
+    },
+    {
+      bg: 8.1,
+      time: '2:30PM',
+      insulin: 7
+    },
+    {
+      bg: 3.1,
+      time: '5:00PM',
+      insulin: 0,
+      note: '25 grams of carbs to correct'
     }
   ]
 
