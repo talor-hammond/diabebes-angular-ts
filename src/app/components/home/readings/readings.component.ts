9ea01faf6c5f6c68960e536ad530f8be0bf5c0d2
talor-hammond <talorjhammond@gmail.com>
2018-08-31 03:09:28 +1200

    rf'd readings data into it's own service

--- src/app/components/home/readings/readings.component.ts
+++ src/app/components/home/readings/readings.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core'
 
-// Model(s)
-import { Reading } from './reading.model'
-
 @Component({
   selector: 'app-readings',
   templateUrl: './readings.component.html',
@@ -10,42 +7,12 @@ import { Reading } from './reading.model'
 })
 export class ReadingsComponent implements OnInit {
   name: string
-  // readings: Reading[] = [] // type: array of 'Reading' objects, defaulting to an empty array
-  readings: Reading[] = [
-    {
-      bg: 6.4,
-      time: '9:00AM',
-      insulin: 4,
-      note: '~30grams of carbs for breakfast'
-    },
-    {
-      bg: 12.2,
-      time: '12:30PM',
-      insulin: 2,
-      note: '4 units / 30g could be too low'
-    },
-    {
-      bg: 8.1,
-      time: '2:30PM',
-      insulin: 7
-    },
-    {
-      bg: 3.1,
-      time: '5:00PM',
-      insulin: 0,
-      note: '25 grams of carbs to correct'
-    }
-  ]
 
   constructor() {
     this.name = 'Talor'
   }
 
   ngOnInit() {
-  }
-
-  onReadingAdded(reading: Reading) {
-    this.readings.push(reading)
   }
 
   // TODO: method to edit reading.note with new content:
--- src/app/components/home/readings/readings.service.ts
+++ src/app/components/home/readings/readings.service.ts
@@ -1,3 +1,35 @@
+// Model(s)
+import { Reading } from './reading.model'
+
 export class ReadingsService {
-    
+  // readings: Reading[] = [] // type: array of 'Reading' objects, defaulting to an empty array
+  readings: Reading[] = [
+    {
+      bg: 6.4,
+      time: '9:00AM',
+      insulin: 4,
+      note: '~30grams of carbs for breakfast'
+    },
+    {
+      bg: 12.2,
+      time: '12:30PM',
+      insulin: 2,
+      note: '4 units / 30g could be too low'
+    },
+    {
+      bg: 8.1,
+      time: '2:30PM',
+      insulin: 7
+    },
+    {
+      bg: 3.1,
+      time: '5:00PM',
+      insulin: 0,
+      note: '25 grams of carbs to correct'
+    }
+  ]
+
+  onReadingAdded(reading: Reading) {
+    this.readings.push(reading)
+  }
 }
