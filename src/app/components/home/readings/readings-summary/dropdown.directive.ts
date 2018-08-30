e435b6e665a5d8dfac3f68c6a787a5633792b97d
talor-hammond <talorjhammond@gmail.com>
2018-08-30 17:56:15 +1200

    pointer shows on hover

--- src/app/components/home/readings/readings-summary/dropdown.directive.ts
+++ src/app/components/home/readings/readings-summary/dropdown.directive.ts
@@ -1,8 +1,12 @@
-import { Directive } from '@angular/core'
+import { Directive, HostListener } from '@angular/core'
 
 @Directive({
     selector: '[summaryDropdown]'
 })
 export class DropdownDirective {
-    
+    isOpen: boolean = false
+
+    @HostListener('click') toggleSummary() { // @HostListener decorator listens for a 'click' event on the element the directive is applied to, then fires the attached method
+        this.isOpen = !this.isOpen
+    }
 }
