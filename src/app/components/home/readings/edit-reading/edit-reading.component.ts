e7330cb9e2f473166329986a131b4f8a7339e6d7
talor-hammond <talorjhammond@gmail.com>
2018-09-06 21:37:54 +1200

    deactivateModal method working for all readings

--- src/app/components/home/readings/edit-reading/edit-reading.component.ts
+++ src/app/components/home/readings/edit-reading/edit-reading.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { EditModalService } from '../../../shared/edit-modal-service/edit-modal.service'
+
 @Component({
   selector: 'app-edit-reading',
   templateUrl: './edit-reading.component.html',
@@ -8,10 +10,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class EditReadingComponent implements OnInit {
   @Input() index: number
 
-  constructor() { }
+  constructor(private editModalService: EditModalService) { }
 
   ngOnInit() {
     console.log('modal open with index: ', this.index)
+  }
+
+  closeModal() {
+    this.editModalService.deactivateModal()
   }
 
 }
