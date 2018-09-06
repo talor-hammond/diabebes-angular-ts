38ad351e179ecb054823a7b780d1745faa9b98b2
talor-hammond <talorjhammond@gmail.com>
2018-09-06 15:33:33 +1200

    Modal interface for type-requirements

--- src/app/components/shared/edit-modal-service/edit-modal.service.ts
+++ src/app/components/shared/edit-modal-service/edit-modal.service.ts
@@ -1,11 +1,17 @@
 import { Injectable, EventEmitter } from '@angular/core'
 
+// modal interface
+export interface Modal {
+    index: number,
+    isActive: boolean
+}
+
 @Injectable({ providedIn: 'root' }) // configuring our service to 'wrap' / or provide to the entire app
 export class EditModalService {
-    activeModal: {index: number} // look at interfacing this for clarity
-    activeModalUpdated = new EventEmitter<{index: number}>()
+    activeModal: Modal // look at interfacing this for clarity
+    activeModalUpdated = new EventEmitter<Modal>()
 
-    activateModal(modal: {index: number}) {
+    activateModal(modal: Modal) {
         console.log('service firing with modal: ', modal)
         this.activeModal = modal
 
