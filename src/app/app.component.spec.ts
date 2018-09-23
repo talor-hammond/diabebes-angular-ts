e6a86e189f7ad7e34f7644d23dfced7d6218e3f7
talor-hammond <talorjhammond@gmail.com>
2018-09-23 22:04:19 +1200

    fixed setup for app comp spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,47 @@
 import { TestBed, async } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ReadingsComponent } from './components/home/readings/readings.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReadingFormComponent } from './components/home/reading-form/reading-form.component';
+import { ReadingsSummaryComponent } from './components/home/readings/readings-summary/readings-summary.component';
+import { AboutComponent } from './components/about/about.component';
+import { FullReadingComponent } from './components/full-reading/full-reading.component';
+import { EditReadingComponent } from './components/home/readings/edit-reading/edit-reading.component';
+
+import { AppRoutingModule } from './routing/app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        ReadingsComponent,
+        FullReadingComponent,
+        AboutComponent,
+        FooterComponent,
+        ReadingFormComponent,
+        ReadingsSummaryComponent,
+        EditReadingComponent
       ],
+      imports: [
+        AppRoutingModule,
+        BrowserModule,
+        FormsModule,
+      ],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'diabebes-angular-ts'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('diabebes-angular-ts');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to diabebes-angular-ts!');
   }));
 });
