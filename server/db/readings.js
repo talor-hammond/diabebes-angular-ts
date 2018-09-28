39ed32e4a26f404e31602db6e635b821cadca256
talor-hammond <talorjhammond@gmail.com>
2018-09-28 17:38:50 +1200

    method for updating a reading

--- server/db/readings.js
+++ server/db/readings.js
@@ -7,10 +7,18 @@ function getReadings(testDb) {
 }
 
 function addReading(reading, testDb) {
-    const db = testDb = conn;
+    const db = testDb || conn;
 
     return db('readings')
-        .insert(reading)
+        .insert(reading);
+}
+
+function updateReadingById(id, reading, testDb) {
+    const db = testDb || conn;
+
+    return db('readings')
+        .where('id', id)
+        .update(reading);
 }
 
 module.exports = {
