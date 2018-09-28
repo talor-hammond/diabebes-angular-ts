736c134b080840e47bd28c4bf4147700a2918794
talor-hammond <talorjhammond@gmail.com>
2018-09-28 17:36:39 +1200

    method for adding a reading into db

--- server/db/readings.js
+++ server/db/readings.js
@@ -1,11 +1,19 @@
 const conn = require('./connection');
 
 function getReadings(testDb) {
-    const db = testDb || conn
+    const db = testDb || conn;
 
     return db('readings');
 }
 
+function addReading(reading, testDb) {
+    const db = testDb = conn;
+
+    return db('readings')
+        .insert(reading)
+}
+
 module.exports = {
-    getReadings
+    getReadings,
+    addReading
 }
