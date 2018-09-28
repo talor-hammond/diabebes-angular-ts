730f419e1ede3bbc7c26a89df5820e9509350929
talor-hammond <talorjhammond@gmail.com>
2018-09-28 17:32:38 +1200

    complete migration for readings

--- migrations/20180928160325_readings.js
+++ migrations/20180928160325_readings.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('readings', table => {
+    table.increments('id').primary();
+    table.integer('bg');
+    table.integer('insulin');
+    table.string('note');
+    table.timestamp('time').defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('readings');
 };
--- src/app/components/shared/reading.model.ts
+++ src/app/components/shared/reading.model.ts
@@ -1,15 +1,15 @@
 export class Reading {
-    public id: string
-    public bg: number
-    public time: string
-    public insulin?: number
-    public note?: string
+    public id: string;
+    public bg: number;
+    public time: string;
+    public insulin?: number;
+    public note?: string;
 
     constructor(id: string, bg: number, time: string, insulin?: number, note?: string) {
-        this.id = id
-        this.bg = bg
-        this.time = time
-        this.insulin = insulin
-        this.note = note
+        this.id = id;
+        this.bg = bg;
+        this.time = time;
+        this.insulin = insulin;
+        this.note = note;
     }
 }
